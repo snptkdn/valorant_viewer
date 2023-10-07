3141aed758c6c924c98bfb88d152b5f3a8a71545
Ryota KITA <snptkdn@snptkdn-2.local>
2023-10-07 13:37:56 +0900

    統計画面のビューを作成した

--- main.py
+++ main.py
@@ -1,23 +1,40 @@
+import pandas as pd
 import streamlit as st
+import plotly.express as px
 import numpy as np
-import time
 
-progress_bar = st.sidebar.progress(0)
-status_text = st.sidebar.empty()
-last_rows = np.random.randn(1, 1)
-chart = st.line_chart(last_rows)
+# Define the list of players
+players = ['snptkdn', 'job_hoiya', 'tottolu']
 
-for i in range(1, 101):
-    new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
-    status_text.text("%i%% Complete" % i)
-    chart.add_rows(new_rows)
-    progress_bar.progress(i)
-    last_rows = new_rows
-    time.sleep(0.05)
+st.markdown("# Varorant Viewer")
+st.markdown("この画面を一回見るたびに、ユーザーには$1294.6が課金されます。")
 
-progress_bar.empty()
+cont_multi_selected = st.sidebar.multiselect('表示するプレイヤー', players, default=players)
 
-# Streamlit widgets automatically run the script from top to bottom. Since
-# this button is not connected to any other logic, it just causes a plain
-# rerun.
-st.button("Re-run")
+# Create a DataFrame with multiple columns
+df = pd.DataFrame({
+    'snptkdn': [1, 5, 2, 2, 3, 5, 3, 4, 5, 7],
+    'job_hoiya': [2, 4, 1, 6, 3, 2, 4, 5, 6, 8],
+    'tottolu': np.random.randint(1, 10, size=10),
+    'date': pd.date_range('2022-01-01', periods=10, freq='D')
+})
+
+# Create a line chart with multiple lines
+st.line_chart(df.set_index('date')[cont_multi_selected])
+
+df = pd.DataFrame({
+    'snptkdn': [1, 5, 2, 2, 3],
+    'job_hoiya': [2, 4, 1, 6, 3],
+    'tottolu': [2, 4, 1, 6, 3],
+    'category': ['kills', 'deaths', 'assists', 'headshots', 'accuracy']
+})
+
+# Melt the DataFrame to long format
+df_melt = df.melt(id_vars=['category'], var_name='player', value_name='score')
+
+# Filter the DataFrame to selected players
+df_melt = df_melt[df_melt['player'].isin(cont_multi_selected)]
+
+# Create a radar chart with multiple lines
+fig = px.line_polar(df_melt, r='score', theta='category', line_close=True, color='player')
+st.plotly_chart(fig, use_container_width=True)
